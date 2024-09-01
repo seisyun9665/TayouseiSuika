413a3c08e2196311b8538b3f2be1c3d9ac2f6388
yuryr-kili <78632013+yuryr-kili@users.noreply.github.com>
2024-09-01 17:05:43 +0900

    物理演算や衝突判定、プレイヤーの移動などを追加

--- Assets/sayu.cs
+++ Assets/sayu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class sayuu : MonoBehaviour
 {
+    public float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,10 @@ public class sayuu : MonoBehaviour
     void Update()
     {
         if(Input.GetKey(KeyCode.RightArrow)){
-            transform.Translate(1*Time.deltaTime, 0, 0);
+            transform.Translate(1 * Time.deltaTime * speed, 0, 0);
         }
         if(Input.GetKey(KeyCode.LeftArrow)){
-            transform.Translate(-1*Time.deltaTime, 0, 0);
+            transform.Translate(-1*Time.deltaTime * speed, 0, 0);
         }
-        if()
     }
 }
