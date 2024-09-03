c33d65178aba98a47ce1b5ee01e38b2962d2f0a4
yuryr-kili <78632013+yuryr-kili@users.noreply.github.com>
2024-09-03 19:53:15 +0900

    feat/kili add:リンゴを落とす機能を追加

---
+++ Assets/otosu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class otosu : MonoBehaviour
+{
+    public GameObject kudamono;
+    Vector3 initialPositionAdj = new Vector3(-1.2f,-1.2f,0f) ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(kudamono, this.transform.position + initialPositionAdj, this.transform.rotation);
+        }
+    }
+}
