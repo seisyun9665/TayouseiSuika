7d8e1819425cd7360554ba45e19ff559a11d7eee
seisyun9665 <minecraft.syun0521@gmail.com>
2024-09-07 02:56:56 +0900

    clean/seino: move next func to GameManager.cs from Nex.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,29 +5,33 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    /// <summary>全ダイバーシティ</summary>
-    public GameObject[] Diversities;
     /// <summary>シングルトンインスタンス</summary>
     public static GameManager Instance { get; private set; }
+    /// <summary>全ダイバーシティ</summary>
+    public GameObject[] DiversityPrefabs;
+    /// <summary>ネクスト表示用SpriteRenderer</summary>
+    public SpriteRenderer NextSpriteRenderer;
+    /// <summary>Nextに表示するダイバーシティ</summary>
+    private int _NextDiversityIndex;
+    /// <summary>落下させられるダイバーシティの種類数</summary>
+    public int DropDiversityRandomRange = 3;
 
     /// <summary>ゲームオーバー画面</summary>
     public Canvas Canvas_Gameover;
-    
+
     //score関係の変数
     public TMP_Text _scoreText;
     public int score;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         Instance = this;
-        
         score = 0;
-        _scoreText.text = "score : 0";
+        // DropDiversityRandomRangeの不正な値の場合の例外処理
+        DropDiversityRandomRange = DropDiversityRandomRange > DiversityPrefabs.Length ? DiversityPrefabs.Length : DropDiversityRandomRange < 1 ? 1 : DropDiversityRandomRange;
+        _NextDiversityIndex = Random.Range(0, DropDiversityRandomRange);
     }
 
-    // Update is called once per frame
     void Update()
     {
         _scoreText.text = "score : " + score.ToString();
@@ -39,13 +43,13 @@ public class GameManager : MonoBehaviour
     /// <param name="dropPosition">落下の場所</param>
     public void DropDiversity(Vector3 dropPosition)
     {
-        Instantiate(Diversities[GameObject.Find("Next").GetComponent<Next>().bango],
-        dropPosition, Quaternion.identity);
+        // 多様性をランダムに選択して落とす
+        Instantiate(DiversityPrefabs[_NextDiversityIndex], dropPosition, Quaternion.identity);
 
-        GameObject.Find("Next").GetComponent<Next>().Change();
+        // Nextを決定する
+        _NextDiversityIndex = Random.Range(0, DropDiversityRandomRange);
+        NextSpriteRenderer.sprite = DiversityPrefabs[_NextDiversityIndex].GetComponent<SpriteRenderer>().sprite;
     }
-
-
 
     /// <summary>
     /// Canvas_Gameoverを開いて、ゲームオーバー画面を表示する。
@@ -56,9 +60,9 @@ public class GameManager : MonoBehaviour
 
         // ゲームオーバーの処理
         Canvas_Gameover.enabled = true;
-        }
+    }
 
-    
+
     public void ScoreCountUp(int scoreRatio = 1)
     {
         score = score + 10 * scoreRatio * scoreRatio;
@@ -68,5 +72,5 @@ public class GameManager : MonoBehaviour
     {
         return score;
     }
-    
+
 }
