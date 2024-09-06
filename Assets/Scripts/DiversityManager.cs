ffdaeb3975086cd18857af0b8ee1d616012b2913
yuryr-kili <78632013+yuryr-kili@users.noreply.github.com>
2024-09-06 21:53:00 +0900

    feat/kili add:スコア機能を追加

--- Assets/Scripts/DiversityManager.cs
+++ Assets/Scripts/DiversityManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class DiversityManager : MonoBehaviour
 {
     public GameObject nextDiversity;
+    public Scoring scoring;
+
+    public int scoreRatio;
     // Start is called before the first frame update
     void Start()
     {
-
+        scoring = MonoBehaviour.FindObjectOfType<Scoring>();
     }
 
     // Update is called once per frame
@@ -27,6 +32,8 @@ public class DiversityManager : MonoBehaviour
 
             if (nextDiversity != null)
             {
+                Debug.Log(this.gameObject.name);
+                scoring.ScoreCountUp(scoreRatio);
                 Instantiate(nextDiversity, this.transform.position, this.transform.rotation);
             }
 
