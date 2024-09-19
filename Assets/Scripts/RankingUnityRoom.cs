00a68e1e843943340063019c410219ed600f18c2
yuryr-kili <78632013+yuryr-kili@users.noreply.github.com>
2024-09-20 01:12:14 +0900

    feat/kili unityRoomAPIにスコアを送信

--- Assets/Scripts/RankingUnityRoom.cs
+++ Assets/Scripts/RankingUnityRoom.cs
@@ -5,22 +5,19 @@ using unityroom.Api;
 
 public class RankingUnityRoom : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         int currentScore = ScoreManager.Instance.Score;
-        int newRecord = scoreUpdateToUnityRoom(currentScore);
+        scoreUpdateToUnityRoom(currentScore);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    int scoreUpdateToUnityRoom(int currentScore = 0)
+    private void scoreUpdateToUnityRoom(int currentScore = 0)
     {
         UnityroomApiClient.Instance.SendScore(1, currentScore,ScoreboardWriteMode.HighScoreDesc);
-        return 0;
     }
 }
